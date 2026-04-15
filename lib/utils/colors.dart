8558e2993d17da39ee5972e86be9c2a7b8aa898c
mbadrudin170 <mbadrudin170@gmail.com>
2026-04-15 16:26:47 +0000

    a

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+
+// lib/utils/colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Colors.blueAccent;
+  static const Color secondaryColor = Colors.white;
+  static const Color accentColor = Colors.blue;
+  static const Color textColor = Colors.black;
+  static const Color backgroundColor = Colors.white;
+// Tambahkan warna lain yang diperlukan di sini
+}
