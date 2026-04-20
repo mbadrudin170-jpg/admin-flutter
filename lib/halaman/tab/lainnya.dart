a6816073cb8a3d2daf001de8f498769b29500097
mbadrudin170 <mbadrudin170@gmail.com>
2026-04-20 00:12:41 +0000

    s

--- lib/halaman/tab/lainnya.dart
+++ lib/halaman/tab/lainnya.dart
@@ -1,37 +1,67 @@
-// lib/lainnya.dart
+// lib/halaman/tab/lainnya.dart
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:admin/halaman/lainnya/kategori.dart';
 import 'package:admin/halaman/lainnya/paket.dart';
 import 'package:admin/halaman/lainnya/pelanggan.dart';
 
-class LainnyaPage extends StatelessWidget {
+class LainnyaPage extends StatefulWidget {
   const LainnyaPage({super.key});
+
+  @override
+  State<LainnyaPage> createState() => _LainnyaPageState();
+}
+
+class _LainnyaPageState extends State<LainnyaPage> {
+  String _version = '...';
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _version = 'v${info.version} build ${info.buildNumber}';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Lainnya')),
-      // AppBar tidak diperlukan di sini karena sudah ada di main.dart
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
+      body: Column(
         children: [
-          _buildMenuItem(
-            context,
-            icon: Icons.category,
-            title: 'Kategori',
-            page: const KategoriPage(),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(8.0),
+              children: [
+                _buildMenuItem(
+                  context,
+                  icon: Icons.category,
+                  title: 'Kategori',
+                  page: const KategoriPage(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.inventory_2,
+                  title: 'Paket',
+                  page: const PaketPage(),
+                ),
+                _buildMenuItem(
+                  context,
+                  icon: Icons.people,
+                  title: 'Pelanggan',
+                  page: const PelangganPage(),
+                ),
+              ],
+            ),
           ),
-          _buildMenuItem(
-            context,
-            icon: Icons.inventory_2,
-            title: 'Paket',
-            page: const PaketPage(),
-          ),
-          _buildMenuItem(
-            context,
-            icon: Icons.people,
-            title: 'Pelanggan',
-            page: const PelangganPage(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(_version, style: TextStyle(color: Colors.grey[600])),
           ),
         ],
       ),
